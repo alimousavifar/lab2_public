efb092da4603a3c76acb72352783c351d4be6fbe
alimousavifar <ali.mousavifar@gmail.com>
2021-09-26 11:39:00 -0700

    Create pi template

---
+++ MultiThreadPi/MultiThreadPi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MultiThreadPi
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            long numberOfSamples = 1000;
+            long hits;
+            double pi = EstimatePI(numberOfSamples, ref long hits);
+
+            static double EstimatePI(long numberOfSamples, ref long hits)
+            {
+                //implement
+            }
+
+            static double[,] GenerateSamples(long numberOfSamples)
+            {
+                // Implement  
+
+            }
+        }
+    }
+}
