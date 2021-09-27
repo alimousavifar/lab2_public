e6c47866cb1564163e8ba067f14afcab9b7a06b3
RyanmLin <64663423+RyanmLin@users.noreply.github.com>
2021-09-27 15:36:11 -0700

    Mergesort Written and running
    SIngle thread runs for 0 seconds??

--- MergeSort/Program.cs
+++ MergeSort/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Diagnostics;
 
 namespace MergeSort
 {
@@ -7,16 +9,23 @@ namespace MergeSort
         static void Main(string[] args)
         {
 
-            ARRAY_SIZE = 1000;
+            int ARRAY_SIZE = 1000;
             int[] arraySingleThread = new int[ARRAY_SIZE];
-
+            var Rand = new Random();
+            Stopwatch stopWatch = new Stopwatch();
 
 
 
             // TODO : Use the "Random" class in a for loop to initialize an array
+            for (int i = 0; i < arraySingleThread.Length; i++)
+            {
+                arraySingleThread[i] = Rand.Next(0, 99999);
+                //Console.WriteLine(arraySingleThread[i]);
+            }
 
             // copy array by value.. You can also use array.copy()
-            int[] arrayMultiThread = arraySingleThread.Slice(0,arraySingleThread.Length);
+            int[] arrayMultiThread = new int [arraySingleThread.Length];
+            arraySingleThread.CopyTo(arrayMultiThread, 0);
 
             /*TODO : Use the  "Stopwatch" class to measure the duration of time that
                it takes to sort an array using one-thread merge sort and
@@ -25,10 +34,17 @@ namespace MergeSort
 
 
             //TODO :start the stopwatch
-            MergeSort(arraySingleThread);
+            stopWatch.Start();
+            MergeSort(arraySingleThread, 0, arraySingleThread.Length-1);
             //TODO :Stop the stopwatch
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
 
-
+            string timeElapsed = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                                                ts.Hours, ts.Minutes, ts.Seconds,
+                                                ts.Milliseconds / 10);
+            Console.WriteLine("Single Thread time Elapsed: " + timeElapsed);
+            PrintArray(arraySingleThread);
 
             //TODO: Multi Threading Merge Sort
 
@@ -38,18 +54,65 @@ namespace MergeSort
 
 
 
-             /*********************** Methods **********************
-              *****************************************************/
-             /*
-             implement Merge method. This method takes two sorted array and
-             and constructs a sorted array in the size of combined arrays
-             */
+            /*********************** Methods **********************
+             *****************************************************/
+            /*
+            implement Merge method. This method takes two sorted array and
+            and constructs a sorted array in the size of combined arrays
+            */
 
-            static int[] Merge(int[] LA, int[] RA, int[] A)
+            void Merge(int[] array, int left, int mid, int right)
             {
 
                 // TODO :implement
+                // variables for looping
+                int i, j, k = 0;
+                int l = mid - left + 1;
+                int m = right - mid;
 
+                // create temporary arrays
+                int[] Left = new int[l]; 
+                int[] Right = new int[m];
+
+                // Copy left half to left array and
+                // right half to right array
+                for (i = 0; i < l; i++) Left[i] = array[left + i];
+                for (j = 0; j < m; j++) Right[j] = array[mid + 1 + j];
+
+                // Merge the temporary Arrays Together
+                i = 0;
+                j = 0;
+                k = left;// initial index of merged subarray array
+
+                while(i < l && j < m)
+                {
+                    if(Left[i] <= Right[j])
+                    {
+                        array[k] = Left[i];
+                        i++;
+                    }
+                    else
+                    {
+                        array[k] = Right[j];
+                        j++;
+                    }
+                    k++;
+                }
+                
+                // copy the rest of the elements in Left[]
+                while (i < l)
+                {
+                    array[k] = Left[i];
+                    i++;
+                    k++;
+                }
+
+                while (j < m)
+                {
+                    array[k] = Right[j];
+                    j++;
+                    k++;
+                }
             }
 
 
@@ -57,11 +120,18 @@ namespace MergeSort
              implement MergeSort method: takes an integer array by reference
              and makes some recursive calls to intself and then sorts the array
              */
-            static int[] MergeSort(int[] A)
+            void MergeSort(int[] array, int left, int right)
             {
+                // TODO :implement
+                if(left < right)
+                {
+                    int mid = left + (right - left) / 2;
 
-              // TODO :implement
+                    MergeSort(array, left, mid);
+                    MergeSort(array, mid + 1, right);
 
+                    Merge(array, left, mid, right);
+                }
 
             }
 
@@ -72,7 +142,7 @@ namespace MergeSort
                 Console.Write("[");
                 for (int i = 0; i < myArray.Length; i++)
                 {
-                    Console.Write("{0} ", myArray[i]);
+                    Console.WriteLine("{0} ", myArray[i]);
 
                 }
                 Console.Write("]");
@@ -93,7 +163,5 @@ namespace MergeSort
 
 
         }
-
-
     }
 }
