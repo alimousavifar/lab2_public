2599c4678bf9287ffaba4f2b928c1adfa849d0f4
RyanmLin <64663423+RyanmLin@users.noreply.github.com>
2021-10-08 14:58:47 -0700

    Section 2 started

--- MultiThreadPi/MultiThreadPi/Program.cs
+++ MultiThreadPi/MultiThreadPi/Program.cs
@@ -7,19 +7,79 @@ namespace MultiThreadPi
         static void Main(string[] args)
         {
             long numberOfSamples = 1000;
-            long hits;
-            double pi = EstimatePI(numberOfSamples, ref long hits);
+            long hits = 3;
+            double pi;
+            double[,] temp;
+            temp = GenerateSamples(numberOfSamples);
+            Console.WriteLine(temp);
+            hits = hitOrMiss(temp, numberOfSamples);
 
-            static double EstimatePI(long numberOfSamples, ref long hits)
-            {
-                //implement
-            }
+            pi = EstimatePI(numberOfSamples, ref hits);
+            Console.WriteLine("The value of Pi is roughly: " + pi);
 
-            static double[,] GenerateSamples(long numberOfSamples)
-            {
-                // Implement  
-
-            }
+            return;
         }
+        private static double EstimatePI(long numberOfSamples, ref long hits)
+        {
+            //implement
+            double value = 3.14;
+            value = hits / numberOfSamples * 4;
+            return value;
+        }
+        static double[,] GenerateSamples(long numberOfSamples) // returns coordinates
+        {
+            var Rand = new Random();
+            double x, y;
+            // Implement  
+            double[,] hitPointsList = new double[numberOfSamples, 2];
+
+            for (int i = 0; i < numberOfSamples; i++)
+            {
+                hitPointsList[i, 0] = Rand.NextDouble() * 2;
+                hitPointsList[i, 1] = Rand.NextDouble() * 2;
+                //Console.WriteLine( "{0}, {1}", hitPointsList[i, 0], hitPointsList[i, 1]);
+            }
+            return hitPointsList;
+        }
+
+        static long hitOrMiss(double[,] hitPointsList, long numberOfSamples) 
+        {
+            long circle = 0;
+            double x, y;
+            for(int i= 0; i < numberOfSamples; i++)
+            {
+                x = hitPointsList[i, 0];
+                y = hitPointsList[i, 1];
+
+                if (((x - 1) * (x - 1)) + ((y - 1) * (y - 1)) <= 1)
+                {
+                    circle++;
+                }
+            }
+            return circle;
+        }
+        /*private static int RNG()
+        {
+            int PosNeg;
+            int value = 69;
+            var Rand = new Random();
+
+            PosNeg = Rand.Next(0, 15); // generates either 0 or 1
+            
+            Console.WriteLine(PosNeg);
+
+            if(PosNeg == 0)
+            {
+                value = 1;
+                return value;
+            }
+            else
+            {
+                value =-1;
+                return value;
+            }
+            //  return value;
+
+        }*/
     }
 }
